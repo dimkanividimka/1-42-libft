69db6fa1aae3963e0daf3aa3f7b9a2eab2315ec4
Dmytro <velo.dimon.96@gmail.com>
2023-07-01 10:15:50 +0200

    upd on comments

--- ft_memchr.c
+++ ft_memchr.c
@@ -3,15 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dstarov <dstarov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dmytro <dmytro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 20:11:36 by dstarov           #+#    #+#             */
-/*   Updated: 2023/02/13 20:54:12 by dstarov          ###   ########.fr       */
+/*   Updated: 2023/07/01 10:15:14 by dmytro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/* This function searches for a character within a defined memory block
+ * 
+ * Parameters:
+ * - *str: pointer to a start of memory block.
+ * - c: character to search for.
+ * - n: maximum number of elements to be checked.
+ * 
+ * Return:
+ * - pointer to the first occurrence of the character.
+ * - NULL if the character wasn't found. 
+ */
 void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t		i;
